86f97ca5d2a08800dc5c26b08e198d656fe4e2fb
molinanicolas30@gmail.com <molinanicolas30@gmail.com>
2023-08-07 15:14:17 -0300

    hice el div, agregue funcion y estilo

--- funciones.js
+++ funciones.js
@@ -3,8 +3,14 @@ window.addEventListener('load', inicio);
 function inicio(){
    const boton = document.querySelector("button");
    boton.onclick = saludar;
+   const elDiv = document.querySelector("div");
+   elDiv.onclick = saludar2;
 }
 
 function saludar(){
     alert('Hola!');
+}
+
+function saludar2(){
+    alert("Hola! Soy el div");
 }
